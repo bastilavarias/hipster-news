a825f0b46a84d1f23f44c5633963de31ddf10a5d
Sebastian Curtis Lavarias <slavarias@chanzglobal.com>
2023-01-26 18:31:09 +0800

    Web client: Naive UI setup

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -1,53 +1,31 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <n-grid x-gap="12" :cols="4">
+    <n-gi>
+      <div class="light-green" />
+    </n-gi>
+    <n-gi>
+      <div class="green" />
+    </n-gi>
+    <n-gi>
+      <div class="light-green" />
+    </n-gi>
+    <n-gi>
+      <div class="green" />
+    </n-gi>
+  </n-grid>
 </template>
 
+<script setup>
+import { NGrid, NGi } from "naive-ui";
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.light-green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.12);
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.24);
 }
 </style>
